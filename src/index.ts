cb6070be82e4d6b039298a34017975b5a72f1f86
AIAIAI-aii <aisyahtijan@gmail.com>
2026-02-25 04:42:08 +0000

    Update index

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+function hello(nama: string, umur: number): string{
+    return `Selamat Datang ${nama} di PWL, umur ${nama} adalah ${umur} tahun`;
+}
+
+const mahasiswa = "Aisyah";
+const umur:number = 17;
+
+console.log(hello(mahasiswa, umur));
